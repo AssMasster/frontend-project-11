20bae194dfaf6a59f2781aab720c35647574b67d
Ivan Kozelskij <ivankozelskij@gmail.com>
2025-10-27 19:35:49 +0300

    отладка

--- src/lib/validator.js
+++ src/lib/validator.js
@@ -1,14 +1,8 @@
 import * as yup from "yup";
 
 export function validateUrl(url, existingUrls, i18nInstance) {
-  const translations = i18nInstance.getResourceBundle(
-    i18nInstance.language,
-    "translation"
-  );
-  console.log("Все переводы:", translations);
-  const schema = yup.string();
-
-  schema
+  const schema = yup
+    .string()
     .required(i18nInstance.t("ui.errors.required"))
     .url(i18nInstance.t("ui.errors.invalidUrl"))
     .notOneOf(existingUrls, i18nInstance.t("ui.errors.duplicate"));
