ad059a715807cfb82ce53167cb674d75d1ea433f
Ivan Kozelskij <ivankozelskij@gmail.com>
2025-10-27 19:33:09 +0300

    отладка

--- src/lib/validator.js
+++ src/lib/validator.js
@@ -6,8 +6,9 @@ export function validateUrl(url, existingUrls, i18nInstance) {
     "translation"
   );
   console.log("Все переводы:", translations);
-  const schema = yup
-    .string()
+  const schema = yup.string();
+
+  schema
     .required(i18nInstance.t("ui.errors.required"))
     .url(i18nInstance.t("ui.errors.invalidUrl"))
     .notOneOf(existingUrls, i18nInstance.t("ui.errors.duplicate"));
