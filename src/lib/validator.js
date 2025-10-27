ab6aedf73cdb4723dbd602d2d2c78e42f66677f8
Ivan Kozelskij <ivankozelskij@gmail.com>
2025-10-27 19:13:10 +0300

    отладка

--- src/lib/validator.js
+++ src/lib/validator.js
@@ -1,11 +1,16 @@
-import * as yup from 'yup'
+import * as yup from "yup";
 
 export function validateUrl(url, existingUrls, i18nInstance) {
+  console.log("🔍 Yup получает переводы:", {
+    required: i18nInstance.t("ui.errors.required"),
+    invalidUrl: i18nInstance.t("ui.errors.invalidUrl"),
+    duplicate: i18nInstance.t("ui.errors.duplicate"),
+  });
   const schema = yup
     .string()
-    .required(i18nInstance.t('ui.errors.required'))
-    .url(i18nInstance.t('ui.errors.invalidUrl'))
-    .notOneOf(existingUrls, i18nInstance.t('ui.errors.duplicate'))
+    .required(i18nInstance.t("ui.errors.required"))
+    .url(i18nInstance.t("ui.errors.invalidUrl"))
+    .notOneOf(existingUrls, i18nInstance.t("ui.errors.duplicate"));
 
-  return schema.validate(url)
+  return schema.validate(url);
 }
