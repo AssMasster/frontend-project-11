6be2abdc226e9da6362359df7479a377a368ff5a
Ivan Kozelskij <ivankozelskij@gmail.com>
2025-10-27 20:22:10 +0300

    отладка

--- src/lib/validator.js
+++ src/lib/validator.js
@@ -4,7 +4,7 @@ export function validateUrl(url, existingUrls, i18nInstance) {
   const schema = yup
     .string()
     .required(i18nInstance.t("ui.errors.required"))
-    .url(i18nInstance.t("ui.errors.invalidRss"))
+    .url(i18nInstance.t("ui.errors.invalidUrl"))
     .notOneOf(existingUrls, i18nInstance.t("ui.errors.duplicate"));
 
   return schema.validate(url);
